5b5538ca9fd9f6dd7d4f75dd7b5aa7fe76402908
Pannu786 <thepannu786@gmail.com>
2022-03-19 13:43:42 +0530

    added search Icon and css files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
 
 import './App.css';
+import SearchIcon from './search.svg';
+
+const API_URL = 'http://www.omdbapi.com/?apikey=71ee1e37';
 
 function App() {
-  return (
-    <div className="App">
-      <p>hi</p>
-    </div>
-  );
+  //* this is a fetching logic that will fetch the data from the API
+  const searchMovies = async (movie) => {
+    const res = await fetch(`${API_URL}&s=${movie}`);
+    const data = await res.json();
+
+    console.table(data.Search[2]);
+  };
+
+  //* this effect will automatically render the searchMovies function
+  useEffect(() => {
+    searchMovies('american pie');
+  }, []);
+
+  return <h1>hi</h1>;
 }
 
 export default App;
